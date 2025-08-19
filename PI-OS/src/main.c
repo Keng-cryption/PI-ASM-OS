f4e44897bb32f89853d4e0742782d56d78d67bfc
Keng_cryption <149893623+Keng-cryption@users.noreply.github.com>
2025-08-19 13:09:41 -0400

    Update main.c

--- PI-OS/src/main.c
+++ PI-OS/src/main.c
@@ -3,7 +3,7 @@
 
 void kernel_main(void) {
     uart_init();
-    uart_puts("\n\npi-os: hello from EL1 🎉\n");
+    uart_puts("\n\npi-os: hello from EL1\n");
     for (uint64_t i = 0;; ++i) {
         if ((i & 0x3FFFFF) == 0) {
             uart_puts("still alive...\n");
