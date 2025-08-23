b781012c2ffbaa514cedc11437e81b6635a61f89
Keng_cryption <149893623+Keng-cryption@users.noreply.github.com>
2025-08-22 21:39:59 -0400

    Update main.c

--- PI-OS/src/main.c
+++ PI-OS/src/main.c
@@ -1,9 +1,17 @@
 #include <stdint.h>
 #include "uart.h"
+#include "calc.h"
 
 void kernel_main(void) {
     uart_init();
     uart_puts("\n\npi-os: hello from EL1\n");
+
+    int result = calc(12, 4, '*');  // 12 * 4 = 48
+
+    uart_puts("calc result: ");
+    uart_print_int(result);  // You'll need this function if it doesn't exist yet
+    uart_puts("\n");
+
     for (uint64_t i = 0;; ++i) {
         if ((i & 0x3FFFFF) == 0) {
             uart_puts("still alive...\n");
